5987a62b458b0d7d4af0c414ad25666309e2df22
Philip Newborough <corenominal@corenominal.org>
2016-03-12 17:16:29 +0000

    Plugin description

---
+++ iewp-events.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: IEWP Events
+ * Plugin URI: https://github.com/corenominal/iewp-events
+ * Description: A WordPress plugin to provide a custom post type for events.
+ * Author: Philip Newborough
+ * Version: 0.0.1
+ * Author URI: https://corenominal.org
+ */
